8392de8ce0bcae8bd48b1c215fe97b21dd264f26
Manohar Prabhu <manoharagent13@gmail.com>
2014-07-02 20:32:27 +0530

    Added left and right logic

--- js/Game.js
+++ js/Game.js
@@ -6,12 +6,12 @@ var Game = function() {
 	var KEY_RIGHT = 39;
 	var EMPTY = 0;
 
-	var board = [[4,2,4,0],[4,2,4,16],[4,2,2,0],[0,4,0,0]];
+	var board = [[4, 2, 4, 0], [4, 2, 4, 16], [4, 2, 2, 0], [0, 4, 0, 0]];
 
 	this.initializeBoard = function() {
 		for ( i = 0; i < 4; i++) {
 			for ( j = 0; j < 4; j++) {
-				this.setTileEmpty(this.getTileObject(i,j));
+				this.setTileEmpty(this.getTileObject(i, j));
 			}
 		}
 	};
@@ -51,109 +51,178 @@ var Game = function() {
 			}
 		});
 	};
-	
-	this.redrawScreenFromArray = function(){
-		var i=0,j=0;
-		for(i=0;i<4;i++){
-			for(j=0;j<4;j++){
-				if(board[i][j] === EMPTY || board[i][j] === undefined || isNaN(board[i][j])){
-					this.setTileEmpty(this.getTileObject(i,j));
+
+	this.redrawScreenFromArray = function() {
+		var i = 0, j = 0;
+		for ( i = 0; i < 4; i++) {
+			for ( j = 0; j < 4; j++) {
+				if (board[i][j] === EMPTY || board[i][j] === undefined || isNaN(board[i][j])) {
+					this.setTileEmpty(this.getTileObject(i, j));
 					board[i][j] = 0;
 				} else {
 					console.log('Got Value ' + board[i][j]);
-					this.setTileValue(this.getTileObject(i,j),board[i][j]);
+					this.setTileValue(this.getTileObject(i, j), board[i][j]);
 				}
 			}
 		}
 	};
-	
-	this.getTileObject = function(i,j){
-		return $('#'+i+'-'+j);
+
+	this.getTileObject = function(i, j) {
+		return $('#' + i + '-' + j);
 	};
-	
-	
+
 	this.upEvent = function() {
-		for(col=0;col<4;col++){
+		for ( col = 0; col < 4; col++) {
 			var tempArray = [];
-				tempArray = [];
-			
-			for(row=0;row<4;row++){
-				if(board[row][col] != 0){
+			tempArray = [];
+
+			for ( row = 0; row < 4; row++) {
+				if (board[row][col] != 0) {
 					tempArray.push(board[row][col]);
-				}	
-			}
-			for(i=0;i<tempArray.length-1;i++){
-				if(tempArray[i] === tempArray[i+1]){
-					tempArray[i] = tempArray[i]+tempArray[i];
-					tempArray[i+1] = 0;
 				}
 			}
-			
+			for ( i = 0; i < tempArray.length - 1; i++) {
+				if (tempArray[i] === tempArray[i + 1]) {
+					tempArray[i] = tempArray[i] + tempArray[i];
+					tempArray[i + 1] = 0;
+				}
+			}
+
 			var pointer = 0;
-			for(i=0;i<tempArray.length;i++){
-				if(tempArray[i]!=0){
+			for ( i = 0; i < tempArray.length; i++) {
+				if (tempArray[i] != 0) {
 					tempArray[pointer] = tempArray[i];
 					pointer++;
 				}
 			}
-			while(pointer < tempArray.length){
+			while (pointer < tempArray.length) {
 				tempArray[pointer] = 0;
 				pointer++;
 			}
-			
-			for(i=0;i<4;i++){
+
+			for ( i = 0; i < 4; i++) {
 				board[i][col] = tempArray[i];
 			}
-			
+
 		}
 		console.log(board);
 		this.redrawScreenFromArray();
 	};
-	
+
 	this.downEvent = function() {
-		for(col=0;col<4;col++){
+		for ( col = 0; col < 4; col++) {
 			var tempArray = [];
-				tempArray = [];
-			
-			for(row=3;row>=0;row--){
-				if(board[row][col] != 0){
+			tempArray = [];
+
+			for ( row = 3; row >= 0; row--) {
+				if (board[row][col] != 0) {
 					tempArray.push(board[row][col]);
-				}	
-			}
-			for(i=0;i<tempArray.length-1;i++){
-				if(tempArray[i] === tempArray[i+1]){
-					tempArray[i] = tempArray[i]+tempArray[i];
-					tempArray[i+1] = 0;
 				}
 			}
-			
+			for ( i = 0; i < tempArray.length - 1; i++) {
+				if (tempArray[i] === tempArray[i + 1]) {
+					tempArray[i] = tempArray[i] + tempArray[i];
+					tempArray[i + 1] = 0;
+				}
+			}
+
 			var pointer = 0;
-			for(i=0;i<tempArray.length;i++){
-				if(tempArray[i]!=0){
+			for ( i = 0; i < tempArray.length; i++) {
+				if (tempArray[i] != 0) {
 					tempArray[pointer] = tempArray[i];
 					pointer++;
 				}
 			}
-			while(pointer < tempArray.length){
+			while (pointer < tempArray.length) {
 				tempArray[pointer] = 0;
 				pointer++;
 			}
-			
-			for(i=3;i>=0;i--){
-				board[i][col] = tempArray[3-i];
+
+			for ( i = 3; i >= 0; i--) {
+				board[i][col] = tempArray[3 - i];
 			}
-			
+
 		}
 		console.log(board);
 		this.redrawScreenFromArray();
 	};
-	
+
 	this.leftEvent = function() {
-		alert('left');
+		for ( row = 0; row < 4; row++) {
+			var tempArray = [];
+			tempArray = [];
+
+			for ( col = 0; col < 4; col++) {
+				if (board[row][col] != 0) {
+					tempArray.push(board[row][col]);
+				}
+			}
+			for ( i = 0; i < tempArray.length - 1; i++) {
+				if (tempArray[i] === tempArray[i + 1]) {
+					tempArray[i] = tempArray[i] + tempArray[i];
+					tempArray[i + 1] = 0;
+				}
+			}
+
+			var pointer = 0;
+			for ( i = 0; i < tempArray.length; i++) {
+				if (tempArray[i] != 0) {
+					tempArray[pointer] = tempArray[i];
+					pointer++;
+				}
+			}
+			while (pointer < tempArray.length) {
+				tempArray[pointer] = 0;
+				pointer++;
+			}
+
+			for ( i = 0; i < 4; i++) {
+				board[row][i] = tempArray[i];
+			}
+
+		}
+		console.log(board);
+		this.redrawScreenFromArray();
+
 	};
-	
+
 	this.rightEvent = function() {
-		alert('right');
+		for ( row = 0; row < 4; row++) {
+			var tempArray = [];
+			tempArray = [];
+
+			for ( col = 3; col >= 0; col--) {
+				if (board[row][col] != 0) {
+					tempArray.push(board[row][col]);
+				}
+			}
+			for ( i = 0; i < tempArray.length - 1; i++) {
+				if (tempArray[i] === tempArray[i + 1]) {
+					tempArray[i] = tempArray[i] + tempArray[i];
+					tempArray[i + 1] = 0;
+				}
+			}
+
+			var pointer = 0;
+			for ( i = 0; i < tempArray.length; i++) {
+				if (tempArray[i] != 0) {
+					tempArray[pointer] = tempArray[i];
+					pointer++;
+				}
+			}
+			while (pointer < tempArray.length) {
+				tempArray[pointer] = 0;
+				pointer++;
+			}
+
+			for ( i = 3; i >= 0; i--) {
+				board[row][i] = tempArray[3 - i];
+			}
+
+		}
+		console.log(board);
+		this.redrawScreenFromArray();
+
 	};
-	
+
 };
